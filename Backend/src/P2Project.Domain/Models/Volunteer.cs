5864a57342a3b3a5c8a87797e4264829ab22f1a5
Oleg Vorontsov <oleg-voroncov@mail.ru>
2024-10-31 16:16:34 +0300

    model Volunteer added

--- Backend/src/P2Project.Domain/Models/Volunteer.cs
+++ Backend/src/P2Project.Domain/Models/Volunteer.cs
@@ -1,20 +1,92 @@
 ﻿
+using CSharpFunctionalExtensions;
 using P2Project.Domain.IDs;
+using P2Project.Domain.ValueObjects;
 
 namespace P2Project.Domain.Models
 {
+    public enum Gender
+    {
+        Male,
+        Female
+    }
     public class Volunteer : Shared.Entity<VolunteerId>
     {
         private Volunteer(VolunteerId id) : base(id) { }
+        private readonly List<Pet> _pets = [];
+        private readonly List<SocialNetwork> _socialNetworks = [];
+        private readonly List<AssistanceDetail> _assistanceDetails = [];
 
         public VolunteerId VolunteerId { get; private set; }
         public string FirstName { get; private set; } = default!;
         public string SecondName { get; private set; } = default!;
         public string LastName { get; private set; } = default!;
+        public int Age { get; private set; }
+        public Gender Gender { get; private set; }
         public string Email { get; private set; } = default!;
         public string Description { get; private set; } = default!;
+        public DateTime RegisteredDate { get; private set; }
+        public double YearsOfExperience => GetYearsOfExperience();
+        public IReadOnlyList<Pet> Pets => _pets;
+        public int NeedsHelpPets => _pets.Count(p => p.Status == AssistanceStatus.NeedsHelp);
+        public int NeedsFoodPets => _pets.Count(p => p.Status == AssistanceStatus.NeedsFood);
+        public int OnMedicationPets => _pets.Count(p => p.Status == AssistanceStatus.OnMedication);
+        public int LooksForHomePets => _pets.Count(p => p.Status == AssistanceStatus.LooksForHome);
+        public int FoundHomePets => _pets.Count(p => p.Status == AssistanceStatus.FoundHome);
+        public string PhoneNumber {  get; private set; } = default!;
+        public IReadOnlyList<SocialNetwork> SocialNetworks => _socialNetworks;
+        public IReadOnlyList<AssistanceDetail> AssistanceDetails => _assistanceDetails;
+        private Volunteer(VolunteerId volunteerId,
+                          string firstName,
+                          string secondName,
+                          string lastName,
+                          int age,
+                          Gender gender,
+                          string email,
+                          string description,
+                          DateTime registeredDate) : base(volunteerId)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            LastName = lastName;
+            Age = age;
+            Gender = gender;
+            Email = email;
+            Description = description;
+            RegisteredDate = registeredDate;
+        }
+        public static Result<Volunteer> Create(VolunteerId volunteerId,
+                                               string firstName,
+                                               string secondName,
+                                               string lastName,
+                                               int age,
+                                               Gender gender,
+                                               string email,
+                                               string description,
+                                               DateTime registeredDate)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                return Result.Failure<Volunteer>("FirstName can't be empty");
+            }
+            if (string.IsNullOrWhiteSpace(secondName))
+            {
+                return Result.Failure<Volunteer>("SecondName can't be empty");
+            }
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                return Result.Failure<Volunteer>("LastName can't be empty");
+            }
 
+            var volunteer = new Volunteer(volunteerId, firstName, secondName, lastName, age,
+                                          gender, email, description, registeredDate);
 
-
+            return Result.Success(volunteer);
+        }
+        private double GetYearsOfExperience()
+        {
+            var timeSpan = DateTime.Now - RegisteredDate;
+            return timeSpan.TotalDays / 365.25;
+        }
     }
 }
--- Backend/src/P2Project.Domain/ValueObjects/AssistanceStatus.cs
+++ Backend/src/P2Project.Domain/ValueObjects/AssistanceStatus.cs
@@ -1,14 +1,31 @@
 ﻿using CSharpFunctionalExtensions;
+using System.Linq;
 
 namespace P2Project.Domain.ValueObjects
 {
     public class AssistanceStatus : ValueObject
     {
-        public string Status { get; }
+        public string Status { get; } = default!;
+
+        private readonly List<AssistanceStatus> _statusList = [NeedsHelp,
+                                                               NeedsFood,
+                                                               OnMedication,
+                                                               LooksForHome,
+                                                               FoundHome];
+
+        public IReadOnlyList<AssistanceStatus> StatusList => _statusList;
+        public static AssistanceStatus NeedsHelp { get; } = default!;
+        public static AssistanceStatus NeedsFood { get; } = default!;
+        public static AssistanceStatus OnMedication { get; } = default!;
+        public static AssistanceStatus LooksForHome { get; } = default!;
+        public static AssistanceStatus FoundHome { get; } = default!;
+
         private AssistanceStatus(string status)
         {
             Status = status;
+            _statusList.Add(this);
         }
+
         public static Result<AssistanceStatus> Create(string status)
         {
             if (string.IsNullOrWhiteSpace(status))
@@ -18,6 +35,7 @@ namespace P2Project.Domain.ValueObjects
 
             return Result.Success(newStatus);
         }
+
         protected override IEnumerable<object> GetEqualityComponents()
         {
             throw new NotImplementedException();
