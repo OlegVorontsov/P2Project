693472989b291a49b48c4cb18d96311b5ace2659
Oleg Vorontsov <oleg-voroncov@mail.ru>
2024-10-31 12:54:36 +0300

    branch ID-B-3 created

---
+++ Backend/src/P2Project.Domain/Models/Volunteer.cs
@@ -0,0 +1,20 @@
+﻿
+using P2Project.Domain.IDs;
+
+namespace P2Project.Domain.Models
+{
+    public class Volunteer : Shared.Entity<VolunteerId>
+    {
+        private Volunteer(VolunteerId id) : base(id) { }
+
+        public VolunteerId VolunteerId { get; private set; }
+        public string FirstName { get; private set; } = default!;
+        public string SecondName { get; private set; } = default!;
+        public string LastName { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+        public string Description { get; private set; } = default!;
+
+
+
+    }
+}
