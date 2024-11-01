6651d1ec92b693deea862b45fbf55c2bb583c262
Oleg Vorontsov <oleg-voroncov@mail.ru>
2024-11-01 11:26:55 +0300

    branch ID-B-4.1 created

---
+++ Backend/src/P2Project.Infrastructure/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2Project.Domain.Models;
+
+namespace P2Project.Infrastructure
+{
+    public class ApplicationDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("");
+        }
+        public DbSet<Volunteer> Volunteers => Set<Volunteer>();
+    }
+}
