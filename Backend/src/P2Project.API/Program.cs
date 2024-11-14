0f10f15d59bb102310c61f5296edc0a1bb404a12
Oleg Vorontsov <oleg-voroncov@mail.ru>
2024-11-14 12:52:20 +0300

    seq with docker compose added

--- Backend/src/P2Project.API/Program.cs
+++ Backend/src/P2Project.API/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .Enrich.WithThreadName()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") ??
+        throw new ArgumentNullException("Seq"))
     .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
     .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
     .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
