83571464aae9e0d5a6093ff755db1c835d765ab8
Oleg Vorontsov <oleg-voroncov@mail.ru>
2024-11-22 18:03:51 +0300

    create pet handler doing

--- Backend/src/P2Project.Application/Volunteers/CreatePet/CreatePetRequest.cs
+++ Backend/src/P2Project.Application/Volunteers/CreatePet/CreatePetRequest.cs
@@ -2,9 +2,15 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using P2Project.Application.Shared.Dtos;
+using P2Project.Application.Species;
+using P2Project.Application.Species.Create;
 using P2Project.Domain.PetManagment.Entities;
+using P2Project.Domain.PetManagment.ValueObjects;
 using P2Project.Domain.Shared;
 using P2Project.Domain.Shared.IDs;
+using P2Project.Domain.SpeciesManagment.Entities;
+using P2Project.Domain.SpeciesManagment.ValueObjects;
+using System.Xml.Linq;
 
 namespace P2Project.Application.Volunteers.CreatePet
 {
@@ -47,7 +53,24 @@ namespace P2Project.Application.Volunteers.CreatePet
 
     public record FileDto(string FileName);
 
-    public record CreatePetCommand(CreatePetRequest Request);
+    public record CreatePetCommand(
+        Guid VolunteerId,
+        string NickName,
+        string Species,
+        string Breed,
+        string? Description,
+        string Color,
+        string? HealthInfo,
+        AddressDto Address,
+        double Weight,
+        double Height,
+        PhoneNumberDto OwnerPhoneNumber,
+        bool IsCastrated,
+        bool IsVaccinated,
+        DateOnly DateOfBirth,
+        string AssistanceStatus,
+        IEnumerable<AssistanceDetailDto>? AssistanceDetails,
+        IEnumerable<FileDto> PetPhotos);
 
     public class CreatePetValidator
     {
@@ -60,26 +83,100 @@ namespace P2Project.Application.Volunteers.CreatePet
     public class CreatePetHandler
     {
         private readonly IVolunteersRepository _volunteersRepository;
-        private readonly ILogger<CreatePetHandler> _logger;
+        private readonly ISpeciesRepository _speciesRepository;
+        private readonly ILogger<CreatePetHandler> _petLogger;
+        private readonly ILogger<CreateHandler> _speciesLogger;
 
         public CreatePetHandler(
             IVolunteersRepository volunteersRepository,
-            ILogger<CreatePetHandler> logger)
+            ISpeciesRepository speciesRepository,
+            ILogger<CreatePetHandler> petLogger,
+            ILogger<CreateHandler> speciesLogger)
         {
             _volunteersRepository = volunteersRepository;
-            _logger = logger;
+            _speciesRepository = speciesRepository;
+            _petLogger = petLogger;
+            _speciesLogger = speciesLogger;
         }
         public async Task<Result<Guid, Error>> Handle(
             CreatePetCommand command,
             CancellationToken cancellationToken = default)
         {
             var volunteerId = VolunteerId.CreateVolunteerId(
-                command.Request.VolunteerId);
+                command.VolunteerId);
 
             var volunteerResult = await _volunteersRepository.GetById(
                 volunteerId, cancellationToken);
             if (volunteerResult.IsFailure)
-                return Errors.General.NotFound(command.Request.VolunteerId);
+                return Errors.General.NotFound(command.VolunteerId);
+
+            var petId = PetId.NewPetId();
+            var nicKName = NickName.Create(command.NickName).Value;
+
+            var speciesName = Name.Create(command.Species).Value;
+
+            var speciesIfExist = await _speciesRepository.GetByName(
+                speciesName, cancellationToken);
+
+            if(speciesIfExist.IsFailure)
+            {
+                var newBreeds = new List<Breed>();
+                if (command.Breed != null)
+                {
+                    var breed = new Breed(Name.Create(command.Breed).Value);
+                    newBreeds.AddRange([breed]);
+                }
+
+                var species = new Domain.SpeciesManagment.Species(
+                    SpeciesId.NewSpeciesId(), speciesName, newBreeds);
+
+                await _speciesRepository.Add(species, cancellationToken);
+
+                _speciesLogger.LogInformation(
+                    "Species created with ID: {id}",
+                    species.Id.Value);
+            }
+            var speciesExist = await _speciesRepository.GetByName(
+                speciesName, cancellationToken);
+            var speciesId = speciesExist.Value.Id;
+
+            var breedId = speciesExist.Value?.Breeds?.Where(b =>
+                b.Name.Value == command.Breed)?.Select(r => r.Id)?.FirstOrDefault();
+
+            if (breedId == null && breedId.Value != Guid.Empty)
+                return Errors.General.ValueIsInvalid(command.Breed);
+
+            var speciesBreed = new SpeciesBreed(speciesId, breedId.Value);
+
+            var description = Description.Create(command.Description);
+            var color = Color.Create(command.Color);
+            var healthInfo = HealthInfo.Create(command.HealthInfo);
+            var address = Address.Create(
+                command.Address.Region,
+                command.Address.City,
+                command.Address.Street,
+                command.Address.House,
+                command.Address.Floor,
+                command.Address.Apartment);
+            var ownerPhoneNumber = PhoneNumber.Create(
+                command.OwnerPhoneNumber.Value,
+                command.OwnerPhoneNumber.IsMain);
+            var assistanceStatus = AssistanceStatus.Create(
+                command.AssistanceStatus);
+
+            var assistanceDetails = new List<AssistanceDetail>();
+            if (command.AssistanceDetails != null)
+            {
+                var details = command.AssistanceDetails.Select(ad =>
+                                                        AssistanceDetail.Create(
+                                                            ad.Name,
+                                                            ad.Description,
+                                                            ad.AccountNumber).Value);
+                assistanceDetails.AddRange(details);
+            }
+            var petAssistanceDetails = new VolunteerAssistanceDetails(assistanceDetails);
+
+
 
             var newPet = new Pet();
 
